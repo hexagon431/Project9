f0a6e802ab9e2a11760ecc4be0eb869984b24e8a
hexagon431 <hexagon431@gmail.com>
2018-04-28 20:40:47 -0600

    Installed Mongoose for database assistance, deleted unused spec file)

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const path = require('path');
+//const mongo = require('mongodb');
+//const MongoClient = mongo.MongoClient;
+const Mongoose = require('mongoose');
+const dbUrl = "mongodb:https//localhost:27017/usermanage";
 
 const app = express();
 const port = process.env.PORT | 3000;
@@ -14,8 +18,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 let userIndex = 0;
-let users = [];
-let selectedUserPosition;
+let userArray = [];
+//let selectedUserPosition;
+let User = null;
+
+Mongoose.connect(dbUrl);
+let db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'there was a connection error: '));
+db.once('open', () => {
+    console.log('connected to database');
+
+    let userSchema = Mongoose.Schema({
+        uid: Number,
+        name: String,
+        email: String,
+        age: Number
+    });
+
+    User = Mongoose.model('User', userSchema);
+
+    User.find((err, users) => {
+        userArray = users;
+
+        userIndex = userArray.length;
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('index.pug');
@@ -26,71 +53,120 @@ app.get('/create', (req, res) => {
 });
 
 app.post('/create', (req, res) => {
-    let user = {
-        uid: userIndex,
+    let newUser = new User({
+        uid:userIndex,
         name: req.body.name,
         email: req.body.email,
         age: req.body.age
-    };
+    });
+    // let user = {
+    //     uid: userIndex,
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     age: req.body.age
+    // };
+
     userIndex++;
 
-    users.push(user);
+    newUser.save((err) => {
+        if (err) return console.error(err);
 
-    res.redirect('/userlist');
+        console.log(`user ${userIndex - 1} added to the database`);
+
+        res.redirect('/userlist');
+    });
+
+    //users.push(user);
 });
 
 app.get('/userlist', (req, res) => {
-    res.render('user-listing', {
-        userList: users
+
+    User.find({uid: req.params.userid}, (err, users) => {
+        if (err) return console.error(err);
+
+        userArray = users;
+
+        console.log(`all existing users successfully queried`);
+
+        res.render('user-listing', {
+            userList: userArray
+        });
     });
 });
 
 app.get('/edit/:userid', (req, res) => {
-    let elementPosition;
 
-    for(let i = 0; i <= users.length; i++){
-        if(users[i].uid == req.params.userid){
-            elementPosition = i;
-            break;
-        }
-    }
+    User.find({uid: req.params.userid}, (err, foundUser) => {
+        if (err) return console.error(err);
 
-    let user = users[elementPosition];
-    selectedUserPosition = elementPosition;
+        console.log(`user ${req.params.userid} found and sent to edit page`);
 
-
-    res.render('edit-user', {
-        editUser: user
+        res.render('edit-user', {
+            editUser: foundUser
+        });
     });
+    // let elementPosition;
+    //
+    // for(let i = 0; i <= users.length; i++){
+    //     if(users[i].uid == req.params.userid){
+    //         elementPosition = i;
+    //         break;
+    //     }
+    // }
+
+    // let user = users[elementPosition];
+    // selectedUserPosition = elementPosition;
 });
 
 app.post('/update/:userid', (req, res) => {
-    users[selectedUserPosition] = {
+    User.update({uid: req.params.userid}, {$set: {
         uid: req.params.userid,
         name: req.body.name,
         email: req.body.email,
         age: req.body.age
-    };
+    }}, (err) => {
+        if (err) return console.error(err);
 
-    selectedUserPosition = -1;
+        console.log(`user ${req.params.userid} updated`);
 
-    res.redirect('/userlist');
+        res.redirect('/userlist');
+    });
+
+    // User.find({uid: req.params.userid}, (err, foundUser) => {
+    //     if (err) return console.error(err);
+    // });
+    // users[selectedUserPosition] = {
+    //     uid: req.params.userid,
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     age: req.body.age
+    // };
+    //
+    // selectedUserPosition = -1;
+
+
 });
 
 app.get('/delete/:userid', (req, res) => {
+    User.remove({uid: req.params.userid}, (err) => {
+        if (err) return console.error(err);
 
-    let elementPosition;
+        console.log(`user ${req.params.userid} deleted`);
 
-    for(let i = 0; i <= users.length; i++){
-        if(users[i].uid == req.params.userid){
-            elementPosition = i;
-            break;
-        }
-    }
-    users.splice(elementPosition, 1);
+        res.redirect('/userlist');
+    });
+    // let elementPosition;
+    //
+    // for(let i = 0; i <= users.length; i++){
+    //     if(users[i].uid == req.params.userid){
+    //         elementPosition = i;
+    //         break;
+    //     }
+    // }
+    // users.splice(elementPosition, 1);
 
 
-    res.redirect('/userlist');
+
 });
 
 app.listen(port);
